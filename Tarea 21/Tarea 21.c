60f74c92d24e2dd44afb5ab1801db52ccfdeb8ee
Irving Ricardo Juárez Vázquez <31520357+irvjuava@users.noreply.github.com>
2017-11-12 12:44:10 -0600

    Add files via upload

---
+++ Tarea 21/Tarea 21.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "letras.h
+
+int main(){
+	
+	fni();
+	fnr();
+	fnv();
+	fni();
+	fnn();
+	fng();	
+}
+
+
+
---
+++ Tarea 21/letras.h
@@ -0,0 +1,17 @@
+#ifndef LETRAS_H
+#define LETRAS_H
+void fni(){
+	printf("IIII\n  I  \n   I\n   I  \nIIII\n");
+}
+void fnr(){
+	printf("RRRR\nR  R\nRRRR\nR RR\nR  R\n");
+}
+void fnv(){
+	printf("V  V\nV  V\nV  V\n VV \n V \n");
+}
+void fnn(){
+	printf("N  N\nNN N\nN NN\nN NN\nN  N\n");}
+void fng(){
+	printf("GGGG\nG   \nG GG\nG  G\nGGGG\n")
+}}
+#endif
